4a4a13ffffd0b5d43f7a4b4f93fd9131140567f2
Yefer-Betta <yefersonhotman@gmail.com>
2026-06-14 17:10:10 -0400

    fix: import useEffect faltante

--- src/components/POS.jsx
+++ src/components/POS.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useCallback, useRef } from 'react';
+import React, { useState, useMemo, useCallback, useRef, useEffect } from 'react';
 import { useMenu } from '../context/MenuContext';
 import { useSettings } from '../context/SettingsContext';
 import { useOrders } from '../context/OrdersContext';
